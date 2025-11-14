6837dfb62ea94f3a40ee2822817628dac601f780
ccicnce113424 <ccicnce113424@gmail.com>
2025-11-15 07:35:43 +0800

    use nixos-cuda instead of flox

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@ rec {
       "https://nix-community.cachix.org"
       "https://numtide.cachix.org"
 
-      "https://cache.flox.dev"
+      "https://cache.nixos-cuda.org"
 
       # "https://cache.garnix.io"
     ];
@@ -59,7 +59,7 @@ rec {
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
 
-      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
 
       # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
